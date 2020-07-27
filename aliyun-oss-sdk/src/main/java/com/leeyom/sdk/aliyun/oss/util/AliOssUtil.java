52d95c580d0437240eeb81f2800dd1bf768d54bc
superleeyom <635709492@qq.com>
2020-07-27 20:09:40 +0800

    优化阿里云oss流式上传文件

--- aliyun-oss-sdk/src/main/java/com/leeyom/sdk/aliyun/oss/util/AliOssUtil.java
+++ aliyun-oss-sdk/src/main/java/com/leeyom/sdk/aliyun/oss/util/AliOssUtil.java
@@ -1,12 +1,13 @@
 package com.leeyom.sdk.aliyun.oss.util;
 
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.model.ObjectMetadata;
-import com.aliyun.oss.model.PutObjectResult;
 import com.leeyom.sdk.aliyun.oss.config.AliOssProperties;
 import com.leeyom.sdk.base.BizException;
 import com.leeyom.sdk.base.Status;
@@ -16,7 +17,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
 
@@ -37,11 +39,6 @@ public class AliOssUtil {
         AliOssUtil.aliOssProperties = aliOssProperties;
     }
 
-    /**
-     * 允许上传文件的大小
-     */
-    private static long MAX_SIZE = 30 * 1024 * 1024;
-
     @PostConstruct
     private void init() {
         try {
@@ -52,40 +49,45 @@ public class AliOssUtil {
         }
     }
 
-    public static void uploadImg2Oss(String url) {
-        File fileOnServer = new File(url);
-        FileInputStream fin;
-        try {
-            fin = new FileInputStream(fileOnServer);
-            String[] split = url.split("/");
-            uploadFile2OSS(fin, split[split.length - 1]);
-        } catch (FileNotFoundException e) {
-            throw new BizException(Status.ERROR, "图片上传失败");
-        }
-    }
-
+    /**
+     * 流式上传
+     *
+     * @param file 文件域
+     * @return 上传后的url
+     */
     public static String uploadImg2Oss(MultipartFile file) {
         String suffix = validateParam(file);
         String name = IdUtil.simpleUUID() + suffix;
+        InputStream inputStream;
         try {
-            InputStream inputStream = file.getInputStream();
-            uploadFile2OSS(inputStream, name);
-            // 返回完整的访问url
-            String imgUrl = getImgUrl(name);
-            if (StrUtil.isBlank(imgUrl)) {
-                throw new BizException(Status.ERROR, "文件上传失败，url为空");
-            }
-            String[] split = imgUrl.split("\\?");
-            if (ArrayUtil.isEmpty(split)) {
-                throw new BizException(Status.ERROR, "文件上传失败，url为空");
-            }
-            return split[0];
-        } catch (Exception e) {
-            throw new BizException(Status.ERROR, "图片上传失败");
+            inputStream = file.getInputStream();
+        } catch (IOException e) {
+            throw new BizException("文件流读取出错");
         }
+        // 上传文件
+        uploadFile2OSS(inputStream, name);
+        // 返回完整的访问url
+        String imgUrl = getImgUrl(name);
+        if (StrUtil.isBlank(imgUrl)) {
+            throw new BizException(Status.ERROR, "文件上传失败，文件url为空");
+        }
+
+        // 去掉url后面的敏感数据，例如 AccessKeyId 等
+        String[] split = imgUrl.split("\\?");
+        if (ArrayUtil.isEmpty(split)) {
+            throw new BizException(Status.ERROR, "文件上传失败，文件url为空");
+        }
+        return split[0];
     }
 
+    /**
+     * 参数校验
+     *
+     * @param file 文件域
+     * @return 文件后缀
+     */
     private static String validateParam(MultipartFile file) {
+        long MAX_SIZE = 30 * 1024 * 1024;
         if (file == null) {
             throw new BizException(Status.ERROR, "文件不能为空");
         }
@@ -104,123 +106,96 @@ public class AliOssUtil {
         return suffix;
     }
 
-    public static String uploadImg2Oss(InputStream inputStream, String fileName) {
-        try {
-            uploadFile2OSS(inputStream, fileName);
-            String imgUrl = getImgUrl(fileName);
-            String[] split = imgUrl.split("\\?");
-            if (ArrayUtil.isEmpty(split)) {
-                throw new BizException(Status.ERROR, "文件上传失败，url为空");
-            }
-            return split[0];
-        } catch (Exception e) {
-            throw new BizException(Status.ERROR, "图片上传失败");
-        }
-    }
-
     /**
      * 获得图片路径
      *
-     * @param fileUrl
-     * @return
+     * @param fileName 文件名，比如：123.jpg
+     * @return 图片的访问路径
      */
-    private static String getImgUrl(String fileUrl) {
-        System.out.println(fileUrl);
-        if (StrUtil.isNotBlank(fileUrl)) {
-            String[] split = fileUrl.split("/");
-            return getUrl(aliOssProperties.getFileDir() + split[split.length - 1]);
+    private static String getImgUrl(String fileName) {
+        if (StrUtil.isNotBlank(fileName)) {
+            return getUrl(aliOssProperties.getFileDir() + fileName);
         }
-        return null;
+        return StrUtil.EMPTY;
     }
 
     /**
      * 上传到OSS服务器 如果同名文件会覆盖服务器上的
      *
-     * @param instream 文件流
-     * @param fileName 文件名称 包括后缀名
-     * @return 出错返回"" ,唯一MD5数字签名
+     * @param inputStream 文件流
+     * @param fileName    文件名称 包括后缀名
      */
-    private static String uploadFile2OSS(InputStream instream, String fileName) {
-        String ret = "";
+    private static void uploadFile2OSS(InputStream inputStream, String fileName) {
         try {
             // 创建上传Object的Metadata
             ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(instream.available());
+            objectMetadata.setContentLength(inputStream.available());
             objectMetadata.setCacheControl("no-cache");
             objectMetadata.setHeader("Pragma", "no-cache");
-            objectMetadata.setContentType(getcontentType(fileName.substring(fileName.lastIndexOf("."))));
+            objectMetadata.setContentType(getContentType(fileName.substring(fileName.lastIndexOf("."))));
             objectMetadata.setContentDisposition("inline;filename=" + fileName);
             // 上传文件
-            PutObjectResult putResult = ossClient.putObject(aliOssProperties.getBucketName(),
-                    aliOssProperties.getFileDir() + fileName, instream, objectMetadata);
-            ret = putResult.getETag();
+            ossClient.putObject(aliOssProperties.getBucketName(), aliOssProperties.getFileDir() + fileName, inputStream, objectMetadata);
         } catch (IOException e) {
-            log.error(e.getMessage(), e);
+            log.error("阿里云oss文件上传失败：", e);
         } finally {
-            try {
-                if (instream != null) {
-                    instream.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            IoUtil.close(inputStream);
         }
-        return ret;
     }
 
     /**
      * 判断OSS服务文件上传时文件的contentType
      *
-     * @param filenameExtension 文件后缀
-     * @return String
+     * @param fileSuffix 文件后缀
+     * @return ContentType
      */
-    private static String getcontentType(String filenameExtension) {
-        if (filenameExtension.equalsIgnoreCase("bmp")) {
+    private static String getContentType(String fileSuffix) {
+        if (fileSuffix.equalsIgnoreCase("bmp")) {
             return "image/bmp";
         }
-        if (filenameExtension.equalsIgnoreCase("gif")) {
+        if (fileSuffix.equalsIgnoreCase("gif")) {
             return "image/gif";
         }
-        if (filenameExtension.equalsIgnoreCase("jpeg") || filenameExtension.equalsIgnoreCase("jpg")
-                || filenameExtension.equalsIgnoreCase("png")) {
+        if (fileSuffix.equalsIgnoreCase("jpeg") || fileSuffix.equalsIgnoreCase("jpg")
+                || fileSuffix.equalsIgnoreCase("png")) {
             return "image/jpeg";
         }
-        if (filenameExtension.equalsIgnoreCase("html")) {
+        if (fileSuffix.equalsIgnoreCase("html")) {
             return "text/html";
         }
-        if (filenameExtension.equalsIgnoreCase("txt")) {
+        if (fileSuffix.equalsIgnoreCase("txt")) {
             return "text/plain";
         }
-        if (filenameExtension.equalsIgnoreCase("vsd")) {
+        if (fileSuffix.equalsIgnoreCase("vsd")) {
             return "application/vnd.visio";
         }
-        if (filenameExtension.equalsIgnoreCase("pptx") || filenameExtension.equalsIgnoreCase("ppt")) {
+        if (fileSuffix.equalsIgnoreCase("pptx") || fileSuffix.equalsIgnoreCase("ppt")) {
             return "application/vnd.ms-powerpoint";
         }
-        if (filenameExtension.equalsIgnoreCase("docx") || filenameExtension.equalsIgnoreCase("doc")) {
+        if (fileSuffix.equalsIgnoreCase("docx") || fileSuffix.equalsIgnoreCase("doc")) {
             return "application/msword";
         }
-        if (filenameExtension.equalsIgnoreCase("xml")) {
+        if (fileSuffix.equalsIgnoreCase("xml")) {
             return "text/xml";
         }
         return "image/jpeg";
     }
 
     /**
-     * 获得url链接
+     * 使用签名URL临时授权
      *
-     * @param key
-     * @return
+     * @param key objectName
+     * @return 文件访问url
      */
     private static String getUrl(String key) {
         // 设置URL过期时间为10年 3600l* 1000*24*365*10
         Date expiration = new Date(System.currentTimeMillis() + 3600L * 1000 * 24 * 365 * 10);
         // 生成URL
         URL url = ossClient.generatePresignedUrl(aliOssProperties.getBucketName(), key, expiration);
-        if (url != null) {
+        if (ObjectUtil.isNotNull(url)) {
             return url.toString();
         }
-        return null;
+        return StrUtil.EMPTY;
     }
 
 }
